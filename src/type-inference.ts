ddadae68d8e2f49de0039efc735141d405f5b060
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-08-01 23:19:15 +0600

    type inference

---
+++ src/type-inference.ts
@@ -0,0 +1,8 @@
+// typescript automatically detect type of a variable
+
+let count = 20;
+// count = "Hello";  // type string is not assignable to type number
+
+let name = "John";
+// name = 23  // type number is not assignable to type string
+
