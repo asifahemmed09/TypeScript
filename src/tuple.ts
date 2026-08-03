152afb1d8cc71de5df2822d75eebeaddd1849b81
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-08-04 01:37:03 +0600

    tuple

---
+++ src/tuple.ts
@@ -0,0 +1,8 @@
+const order: [string, number] = ["T-Shirt", 240]
+
+const highestMarks: [number,number,number] = [99,98,97]
+
+// named tuple
+const user: [name: string, age: number] = ["John",34]
+
+
