42f9723c072058da87c4d5dacd70b8e9f6db2f9f
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-08-04 02:45:57 +0600

    type alias

--- src/interface.ts
+++ src/interface.ts
@@ -1,69 +1,86 @@
-type Order = {
-  type: string;
-  sugar: number;
-  strong: boolean;
-};
-function makeTea(order: Order) {
-  console.log(order);
-}
-function serveTea(order: Order) {
-  console.log(order);
-}
-
-type TeaOrder = {
-  type: string
-  sugar: number
-}
-
-class Tea implements TeaOrder{
-  type = "Masala"
-  sugar = 2
-}
-
-// interface
 interface CupSize {
   size :  "small" | "large"
 }
+
 class Cup implements CupSize{
    size: "small" | "large" = "large"
 }
 
-// union type
-type role = "admin" | "user"
-
-const user: role = "admin"
-
-// intersection type
-type BaseIngredients = {teaLeaves: number}
-type Extra = {ingredients: string}
-type MasalaTea = BaseIngredients & Extra
-
-const myTea: MasalaTea = {
-  teaLeaves: 2,
-  ingredients: "masala"
-}
-
-
-// optional type
-
-type User = {
+interface User {
   name: string
-  bio?: string
+  age: number
+  role: "admin" | "user"
 }
 
-const user1: User = {name:"John"}
-const user2: User = {name:"Jane",bio: "student"}
-
-//  readonly type
-type Config = {
-  readonly appName: string
-  version: number
+const user: User = {
+  name: "John",
+  age: 24,
+  role: "admin"
 }
 
-const configuration: Config = {
-  appName: "My App",
-  version: 1
+interface CalculateDiscount {
+  (price: number): number
 }
 
-// configuration.appName = "My App 2"  // cannot assign a new value to readonly properties
-configuration.version = 2
+const calculateDiscount: CalculateDiscount = (price: number) => price * 0.5
+
+interface TeaMachine {
+  start(): void
+  stop(): void
+}
+
+const teaMachine: TeaMachine = {
+  start() {
+    console.log("Starting")
+  },
+  stop() {
+    console.log("Stopped")
+  }
+}
+
+// merging interface
+interface Item {
+  readonly id: string
+}
+interface Item {
+  name: string
+}
+
+const item: Item = {
+  id: "1",
+  name: "T-Shirt"
+}
+
+
+// extending interface
+interface Longitude {
+  lon: [number, number]
+}
+
+interface Latitude {
+  lat: [number, number]
+}
+
+interface Location extends Longitude, Latitude{
+  name:  string
+}
+
+const currentLocation: Location = {
+  name: "New York",
+  lon: [22.4, 55.6],
+  lat: [44.6, 44.2]
+}
+
+// indexing interface
+interface Tea {
+  [type: string]: number
+}
+
+function getTea(tea: Tea){
+  console.log(tea)
+}
+
+getTea({ginger: 2})
+
+
+
---
+++ src/type-alias.ts
@@ -0,0 +1,63 @@
+type Order = {
+  type: string;
+  sugar: number;
+  strong: boolean;
+};
+function makeTea(order: Order) {
+  console.log(order);
+}
+function serveTea(order: Order) {
+  console.log(order);
+}
+
+type TeaOrder = {
+  type: string
+  sugar: number
+}
+
+class Tea implements TeaOrder{
+  type = "Masala"
+  sugar = 2
+}
+
+
+
+// union type
+type role = "admin" | "user"
+
+const user: role = "admin"
+
+// intersection type
+type BaseIngredients = {teaLeaves: number}
+type Extra = {ingredients: string}
+type MasalaTea = BaseIngredients & Extra
+
+const myTea: MasalaTea = {
+  teaLeaves: 2,
+  ingredients: "masala"
+}
+
+
+// optional type
+
+type User = {
+  name: string
+  bio?: string
+}
+
+const user1: User = {name:"John"}
+const user2: User = {name:"Jane",bio: "student"}
+
+//  readonly type
+type Config = {
+  readonly appName: string
+  version: number
+}
+
+const configuration: Config = {
+  appName: "My App",
+  version: 1
+}
+
+// configuration.appName = "My App 2"  // cannot assign a new value to readonly properties
+configuration.version = 2
