9729dc58f5bf085e43d1c5aa3d7193d026cc6dbd
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-08-04 01:34:06 +0600

    array type

---
+++ src/array-type.ts
@@ -0,0 +1,15 @@
+const marks: number[] = [44,67,86,77,64,]
+
+type User = {
+  name: string
+  age: number
+}
+
+const apps: Array<User> = [{name:"John",age:24},{name:"Jane",age:36}]
+
+const  fruits: readonly string[] = ["apple","banana","mango"]
+
+// multidimensional array
+const matrix: number[][] = [[1,2,3],[4,5,6]]
+
+
