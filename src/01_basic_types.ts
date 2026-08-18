eeb341c740c02945aca952c68efce3e79ac926b2
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-08-18 20:23:55 +0600

    basic types

---
+++ src/01_basic_types.ts
@@ -0,0 +1,30 @@
+// Number
+let num: number = 12
+
+// BigInt
+let bigNum: bigint = 1232456n
+
+// String
+let name: string = "John Doe"
+
+// Undefined
+let data: undefined = undefined
+
+// Null
+let phone: null = null
+
+// Array
+let numbers: number[] = [1,2,3,4,5]
+
+// Object
+let obj: object = {
+  name: "John Doe",
+  age: 24
+}
+
+let user: { id: number; name: string; age: number} = {
+  id: 1,
+  name: "John Doe",
+  age: 24,
+}
+
