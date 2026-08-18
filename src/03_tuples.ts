33a5144e9b50f40223c2848d1ed9f4b5af88ae84
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-08-18 20:46:54 +0600

    tuples

---
+++ src/03_tuples.ts
@@ -0,0 +1,6 @@
+let user: [string, number] = ["John Doe", 24]
+
+let coordinates3D: [number, number, number] = [22,33,44]
+
+let response: [string, string] = ["1","John Doe"]
+response.push("Jane") //! This is a drawback of tuple 
