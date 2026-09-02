7915f17efac887d2444471a42e06ddee1b520b87
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-09-03 01:44:40 +0600

    type inference

---
+++ src/01_type_inference.ts
@@ -0,0 +1,8 @@
+let name = "John Doe"
+// name = 10 //! Type 'number' is not assignable to type 'string'.
+
+let age = 30
+// age = "John Doe" //! Type 'string' is not assignable to type 'number'.
+
+let isMarried =  false
+// isMarried = 2 //! Type 'number' is not assignable to type 'boolean'.
