12f326ddfa6b5f003fedf41007f56e34bd570398
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-09-03 01:53:11 +0600

    primitive types

---
+++ src/02_primitive_types.ts
@@ -0,0 +1,16 @@
+let userName: string = "John Doe"
+
+let userAge: number = 30
+
+let isAdmin: boolean = false
+
+const bigNumber: bigint =  2n ** 6n - 23n
+
+const token: unique symbol = Symbol("token")
+
+function yearsToDay(years: number): number{
+  return years * 365
+}
+
+
+
