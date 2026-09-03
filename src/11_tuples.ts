e28ab17ac6f19338aa8d063322bcdfdeca7261e3
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-09-03 21:14:53 +0600

    tuples

---
+++ src/11_tuples.ts
@@ -0,0 +1,10 @@
+const mix: [string, number] = ["a", 1]
+
+const userEntry: [string, number] = ["John Doe", 24]
+
+
+type ResponseRow = [status: number, message?: string]
+
+const r: ResponseRow = [300]
+
+const coordinates: readonly [number, number] = [0,0]
