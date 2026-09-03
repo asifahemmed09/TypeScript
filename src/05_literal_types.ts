127bbc8d1d2ed32bb450b23ac4e95ab36b7eb376
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-09-03 19:31:41 +0600

    literal types

---
+++ src/05_literal_types.ts
@@ -0,0 +1,12 @@
+type Direction = "left" | "right"
+
+function move(direction: Direction): Direction{
+  return direction
+}
+
+const direction1 = "left"
+move(direction1)
+
+let direction2: Direction = "right"
+move(direction2)
+
